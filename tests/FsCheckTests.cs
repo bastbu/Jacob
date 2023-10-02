2f3529482c9de561ec248f92e685110d9e458986
Bastian Burger <bastian.burger@microsoft.com>
2023-10-02 09:26:32 +0200

    Sketch idea for automatic test generation

--- tests/FsCheckTests.cs
+++ tests/FsCheckTests.cs
@@ -6,16 +6,21 @@ namespace Jacob.Tests;
 
 using FsCheck;
 using FsCheck.Xunit;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Interfaces;
+using MoreLinq.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using Xunit;
+using Xunit.Abstractions;
+using static FsCheck.TestResult;
 
 interface ISerializableObject
 {
     string Serialize();
     IJsonReader<object> GetDeserializer();
-    object Value { get; }
+    Func<object, bool> ValueComparer();
 }
 
 sealed class JsonString : ISerializableObject
@@ -24,11 +29,15 @@ sealed class JsonString : ISerializableObject
 
     public JsonString(string value) => Value = value;
 
-    public string Serialize() => JsonSerializer.Serialize(Value);
+    public string Serialize() => JsonSerializer.Serialize((string)Value);
     public IJsonReader<object> GetDeserializer() => from s in JsonReader.String()
                                                     select (object)s;
 
+    public Func<object, bool> ValueComparer() => Value.Equals;
+
     public static implicit operator JsonString(string value) => new(value);
+
+    public override string ToString() => "String";
 }
 
 sealed class JsonNumber : ISerializableObject
@@ -38,66 +47,88 @@ sealed class JsonNumber : ISerializableObject
     public JsonNumber(int value) => Value = value;
 
 
-    public string Serialize() => JsonSerializer.Serialize(Value);
+    public string Serialize() => JsonSerializer.Serialize((int)Value);
     public IJsonReader<object> GetDeserializer() => from i in JsonReader.Int32()
                                                     select (object)i;
     public static implicit operator JsonNumber(int value) => new(value);
+
+    public Func<object, bool> ValueComparer() => Value.Equals;
+
+    public override string ToString() => "Int32";
 }
 
 sealed class JsonArray : ISerializableObject
 {
     readonly IReadOnlyCollection<ISerializableObject> value;
 
-    public object Value => this.value.Select(v => v.Value).ToList();
-
     public JsonArray(IReadOnlyCollection<ISerializableObject> value) => this.value = value;
 
-    public string Serialize() => JsonSerializer.Serialize(Value);
+    public string Serialize() =>
+        $"[{string.Join(',', from v in this.value select v.Serialize())}]";
+
     public IJsonReader<object> GetDeserializer() =>
-        JsonReader.Array(this.value.FirstOrDefault()?.GetDeserializer() ?? (from i in JsonReader.Int32() select (object)i),
+        JsonReader.Array(this.value.FirstOrDefault()?.GetDeserializer() ?? (from i in JsonReader.String().OrNull() select (object)i!),
                          arr => (object)arr);
+
+    public Func<object, bool> ValueComparer() => o =>
+    {
+        var other = ((IEnumerable<object>)o).ToList();
+
+        if (other.Count != this.value.Count) return false;
+
+        return this.value.Zip(other, (l, r) => l.ValueComparer()(r)).All(b => b);
+    };
+
+    public override string ToString() => $"{this.value}";
 }
 
 public sealed class FsCheckTests
 {
     readonly Gen<ISerializableObject> generator;
+#pragma warning disable IDE0052 // Remove unread private members
+    readonly ITestOutputHelper testOutputHelper;
+#pragma warning restore IDE0052 // Remove unread private members
 
-    public FsCheckTests()
+    public FsCheckTests(ITestOutputHelper testOutputHelper)
     {
-        var primitiveGenerator = Gen.OneOf(new Gen<ISerializableObject>[]
+        var primitiveGenerators = new List<Gen<ISerializableObject>>
         {
             from s in Arb.Generate<string>()
+            where s != null
             select (ISerializableObject)new JsonString(s),
             from i in Arb.Generate<int>()
             select (ISerializableObject)new JsonNumber(i)
-        });
+        };
 
-        var recursiveGenerator =
-            from len in Gen.Choose(0, 10)
-            from l in Gen.ListOf(primitiveGenerator)
-            select (ISerializableObject)new JsonArray(l);
+        var listGenerator =
+            from g in Gen.Sized(s =>
+            {
+                if (s == 0)
+                {
+                    return from el in Gen.ListOf(primitiveGenerators.RandomSubset(1).First())
+                           select (ISerializableObject)new JsonArray(el);
+                }
+                else
+                {
+                    return from el in Gen.ListOf(primitiveGenerators.RandomSubset(1).First())
+                           select (ISerializableObject)new JsonArray(el);
+                }
+            })
+            select g;
 
-        this.generator = Gen.OneOf(primitiveGenerator, recursiveGenerator);
+        // primitiveGenerators.Add(listGenerator);
+
+        this.generator = Gen.OneOf(Gen.OneOf(primitiveGenerators), listGenerator);
+        this.testOutputHelper = testOutputHelper;
     }
 
-    [Fact]
-    public void Tests()
-    {
-        Prop.ForAll(Arb.From(this.generator), arb =>
-        {
-            var deserializer = arb.GetDeserializer();
-            var serialized = arb.Serialize();
-            var deserialized = deserializer.Read(serialized);
-            var serialized2 = JsonSerializer.Serialize(deserialized);
-            return serialized.Equals(serialized2, System.StringComparison.Ordinal);
-        }).VerboseCheckThrowOnFailure();
-    }
-
-    [Property]
-    public Property Success() => Prop.ForAll(Arb.From(this.generator), arb =>
+    [Property(Verbose = true)]
+    public Property ParityWithSystemTextJson() => Prop.ForAll(Arb.From(this.generator), arb =>
     {
         var deserializer = arb.GetDeserializer();
-        var equals = deserializer.Read(arb.Serialize()) == arb;
+        this.testOutputHelper.WriteLine("Serialized value: " + arb.Serialize());
+        this.testOutputHelper.WriteLine("Serialized deserialized value: " + JsonSerializer.Serialize(deserializer.Read(arb.Serialize())));
+        var equals = arb.ValueComparer()(deserializer.Read(arb.Serialize()));
         return equals;
     });
 }
